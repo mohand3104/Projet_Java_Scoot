d796f57d493b06e0cff1940bbf701096c6f5b10e
mazni mohand ou belkacem <121120349+mohand3104@users.noreply.github.com>
2025-04-28 13:44:53 +0200

    Scooter.java

--- Projet Java/Model/Scooter.java
+++ Projet Java/Model/Scooter.java
@@ -1,99 +1,89 @@
-
-import java.io.*;
 import java.util.*;
 
-/**
- * 
- */
 public class Scooter {
     private int id_scooter;
     private int kilometrage;
     private float tarifs;
     private float caution;
     private Parc parc;
-    private Vector<Location>Listlocation=new Vector<Location>();
+    private Vector<Location> Listlocation = new Vector<Location>();
     private Models model;
-    private boolean enLocation; // Ajout de l'attribut pour suivre la location
 
-
-
-    public Scooter(int id_scooter,int kilometrage,float tarifs,float caution,Parc parc,Models model) {
-        this.id_scooter=id_scooter;
-        this.kilometrage=kilometrage;
-        this.tarifs=tarifs;
-        this.caution=caution;
-        this.parc=parc;
-    
-        this.model=model;
+    public Scooter(int id_scooter, int kilometrage, float tarifs, float caution, Parc parc, Models model) {
+        this.id_scooter = id_scooter;
+        this.kilometrage = kilometrage;
+        this.tarifs = tarifs;
+        this.caution = caution;
+        this.parc = parc;
+        this.model = model;
     }
 
-    public void Location(Location location){
+    public void Location(Location location) {
         Listlocation.add(location);
     }
-
-    //getters & setters 
 
     public int getId_Scooter() {
         return id_scooter;
     }
-    
+
     public void setId_Scooter(int id_scooter) {
         this.id_scooter = id_scooter;
     }
-    
+
     public int getKilometrage() {
         return kilometrage;
     }
-    
+
     public void setKilometrage(int kilometrage) {
         this.kilometrage = kilometrage;
     }
-    
+
     public float getTarifs() {
         return tarifs;
     }
-    
+
     public void setTarifs(float tarifs) {
         this.tarifs = tarifs;
     }
-    
+
     public float getCaution() {
         return caution;
     }
-    
+
     public void setCaution(float caution) {
         this.caution = caution;
     }
-    
+
     public Parc getParc() {
         return parc;
     }
-    
+
     public void setParc(Parc parc) {
         this.parc = parc;
     }
-    
+
     public Vector<Location> getListlocation() {
         return Listlocation;
     }
-    
-    
+
     public Models getModels() {
         return model;
     }
-    
+
     public void setModels(Models models) {
-        this.model = model;
+        this.model = models;
     }
 
-    public void setEnLocation(boolean enLocation) {
-        this.enLocation = enLocation;
-    }
-
+    // Calcul automatique de l'état de location
     public boolean isEnLocation() {
-        return enLocation;
+        Date aujourdHui = new Date();
+        for (Location loc : Listlocation) {
+            Date debut = loc.getDateDebut();
+            Date fin = (loc.getRetour() != null) ? loc.getRetour().getDateRetour() : loc.getDateRetourPrevue();
+            if (debut.compareTo(aujourdHui) <= 0 && aujourdHui.compareTo(fin) <= 0) {
+                return true;
+            }
+        }
+        return false;
     }
-    
-    
-
 }
